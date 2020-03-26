ee568f9653d24b383b820d4f75a0c68097fd2f25
HeathManning <heath@manningf.com>
2020-03-26 19:56:26 +1100

    added game

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const IO = require("socket.io");
+const io = require("socket.io");
 const app = express();
 const port = 7070;
 
 app.get("/", function (request, response) {
-    response.send("Hello World!");
+    response.sendFile(__dirname + "/game/index.html");
 });
 
 app.listen(port);
