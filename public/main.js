9baf32588c809063a24ce30bb10bc0dd8363cb5f
HeathManning <heath@manningf.com>
2020-03-27 17:51:42 +1100

    fixed grid system

--- public/main.js
+++ public/main.js
@@ -8,9 +8,11 @@ function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
-let gridSize = new Vec2(32, 32);
-const grid = new Array(gridSize.y);for(let i = gridSize.x; i < gridSize.x; i++){grid[i] = new Array(32);}
 const gridRect = [new Vec2(0, 0), new Vec2(512, 512)];
+const gridSize = new Vec2(32, 32);
+const grid = new Array(gridSize.y);for(let i = gridSize.x; i < gridSize.x; i++){grid[i] = new Array(32);}
+const cellWidth = (gridRect[1].x - gridRect[0].x) / gridSize.x;
+const cellHeight = (gridRect[1].y - gridRect[0].y) / gridSize.y;
 
 function setup()
 {
@@ -23,13 +25,9 @@ function draw()
 
     fill(255);
 
-    let xSize = grid[0].length;
-    let ySize = grid.length;
-    let cellWidth = (gridRect[1].x - gridRect[0].x) / xSize;
-    let cellHeight = (gridRect[1].y - gridRect[0].y) / ySize;
-    for(let y = 0; y < ySize; y++)
+    for(let y = 0; y < gridSize.y; y++)
     {
-        for(let x = 0; x < xSize; x++)
+        for(let x = 0; x < gridSize.x; x++)
         {
             rect(gridRect[0].x + x * cellWidth, gridRect[0].y + y * cellHeight, cellWidth, cellHeight);
         }
