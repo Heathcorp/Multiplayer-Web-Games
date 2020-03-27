9d66f2acfff7e9e8e99d6c0f2abd46a7c3fb056a
HeathManning <heath@manningf.com>
2020-03-27 15:56:01 +1100

    Added p5 and stuff

--- public/main.js
+++ public/main.js
@@ -4,21 +4,16 @@ socket.on("connect", function()
     socket.emit("new player", prompt("Player Name:"));
 });
 
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
 
-
-
-
-//testing lightpath functions
-var lightPath = new LightPath(new Vec2(0, 0), null);
-var lightPath = new LightPath(new Vec2(1, 0), lightPath);
-var lightPath = new LightPath(new Vec2(2, 0), lightPath);
-var lightPath = new LightPath(new Vec2(3, 0), lightPath);
-var lightPath = new LightPath(new Vec2(3, -1), lightPath);
-var lightPath = new LightPath(new Vec2(3, -2), lightPath);
-var lightPath = new LightPath(new Vec2(4, -2), lightPath);
-var lightPath = new LightPath(new Vec2(5, -2), lightPath);
-
-console.log(lightPath.IsOverlapping(new Vec2(0, 1)));
-console.log(lightPath.IsOverlapping(new Vec2(4, -2)));
-
-console.log(lightPath.stringify());
+function setup()
+{
+    createCanvas(window.innerWidth, window.innerHeight);
+}
+  
+function draw()
+{
+    background(32, 32, 48);
+}
