61b24c289dd968e594d0a982c13dea91e4da9040
HeathManning <heath@manningf.com>
2020-03-27 17:49:17 +1100

    added pixel grid

--- public/main.js
+++ public/main.js
@@ -8,6 +8,10 @@ function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
+let gridSize = new Vec2(32, 32);
+const grid = new Array(gridSize.y);for(let i = gridSize.x; i < gridSize.x; i++){grid[i] = new Array(32);}
+const gridRect = [new Vec2(0, 0), new Vec2(512, 512)];
+
 function setup()
 {
     createCanvas(window.innerWidth, window.innerHeight);
@@ -16,4 +20,18 @@ function setup()
 function draw()
 {
     background(32, 32, 48);
+
+    fill(255);
+
+    let xSize = grid[0].length;
+    let ySize = grid.length;
+    let cellWidth = (gridRect[1].x - gridRect[0].x) / xSize;
+    let cellHeight = (gridRect[1].y - gridRect[0].y) / ySize;
+    for(let y = 0; y < ySize; y++)
+    {
+        for(let x = 0; x < xSize; x++)
+        {
+            rect(gridRect[0].x + x * cellWidth, gridRect[0].y + y * cellHeight, cellWidth, cellHeight);
+        }
+    }
 }
